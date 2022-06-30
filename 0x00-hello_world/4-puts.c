152dfd48066ade94732f3c78d9b61630ab9fe42e
Aminax <aaminoxuseen45@gmail.com>
2022-06-30 04:19:00 -0700

    n

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,16 @@
-#include stduio.h
+#include <stdio.h>
 
-int main(){
+/**
+ * main - prints a string
+ * Description: prints "Programming is like building a multilingual puzzle
+ *Return: 0
+ */
+int main(void)
 
+{
 
-puts("\"Programming is like building a multilingual puzzle" );
-return 0
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
