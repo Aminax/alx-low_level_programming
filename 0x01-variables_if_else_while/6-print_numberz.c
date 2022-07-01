e42022b9d9dfa02ced34883e89085902d03a9ba3
Aminax <aaminoxuseen45@gmail.com>
2022-07-01 12:38:11 -0700

    variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints integers
+ * Description: all int are under 10
+ * Return: 0
+ */
+int mian (void)
+{
+int x = 48;
+
+while (x < 58)
+{
+	putchar(x);
+	x++;
+}
+
+putchar('\n');
+return (0);
+}
