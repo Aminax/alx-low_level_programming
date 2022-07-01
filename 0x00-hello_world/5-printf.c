772da3fdf13d13eb604fa49a8ad1c32619bf60fd
Aminax <aaminoxuseen45@gmail.com>
2022-07-01 09:46:17 -0700

    n

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,13 @@
-#incluse <stdio.h>
+#include <stdio.h>
 
-int main (void){
-	
-printf("with proper grammar, but the outcome is a piece of art,\")
-return 0;
+/**
+ * main - print string
+ *
+ *Description:  prints  with proper grammar, but the outcome is a piece of art,
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
-
