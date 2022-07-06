0a45ccbcb4101180937b76f9ea9d2ea69963e4fb
Aminax <aaminoxuseen45@gmail.com>
2022-07-05 19:49:50 -0700

    m

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,12 @@ void print_alphabet_x10(void)
 	int count;
 	int x;
 	
-	count = 0;
-	while (count < 10)
-	{
-		for (x = 97; x <= 122; x++)
-			_putchar(x);
-		_putchar('\n');
-		count++;
-	}
+count = 0;
+while (count < 10)
+{
+	for (x = 97; x <= 122; x++)
+		_putchar(x);
+	_putchar('\n');
+	count++;
+}
 }
