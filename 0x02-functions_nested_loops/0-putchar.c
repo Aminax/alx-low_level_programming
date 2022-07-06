37b1986a594079710eee075d16bec33737d8c8f6
Aminax <aaminoxuseen45@gmail.com>
2022-07-05 17:37:30 -0700

    m

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,7 @@
  * description: the string is putchar
  * return: 0
  */
-int main(void)
+int main(void),
 {
 	_putchar('-');
 	_putchar('p');
@@ -15,7 +15,8 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n')
+	_putchar('\n');
 
 return (0);
 }
+
