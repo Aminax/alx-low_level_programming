e7ab0d233e65cce555efb60389a642af6583daa0
Aminax <aaminoxuseen45@gmail.com>
2022-07-06 01:18:36 -0700

    m

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - prints a 24 hour timer
+ * Description: prints every minute
+ */
+void jack_bauer(void)
+{
+	int time;
+	int x;
+
+	time = 0;
+
+	while (time < 24)
+	{
+		for (x = 0; x < 60; x++)
+		{
+			_putchar(time / 10 + 48);
+			_putchar(time % 10 + 48);
+			_putchar(':');
+			_putchar(x / 10 + 48);
+			_putchar(x % 10 + 48);
+			_putchar('\n');
+		}
+		time++;
+	}
+}
