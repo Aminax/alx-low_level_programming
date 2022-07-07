cb97ee745bb041aa2da6e13223b167446d29fe99
Aminax <aaminoxuseen45@gmail.com>
2022-07-07 02:22:28 -0700

    m

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,15 +3,13 @@
 /**
  * print_numbers - prints the numbers, from 0 to 9, followed by a new line
  *
- */
+ **/
 
 void print_numbers(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
-
 		_putchar('0' + i);
-
 	_putchar('\n');
 }
