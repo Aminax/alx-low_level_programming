a016d4d10a6ce06b255130298a989a2c23c550bf
Aminax <aaminoxuseen45@gmail.com>
2022-07-11 01:57:31 -0700

    l

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,6 @@
-int _putchar(char c)
+#ifndef MAIN_H
+#define MAIN_Hint _putchar(char c)
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -9,3 +11,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+
+#endif
