d3093390b54751a46036cb2db837b90627f9ec38
Aminax <aaminoxuseen45@gmail.com>
2022-07-11 06:24:53 -0700

    what's wrong

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
-/*
+/**
  * reset_to_98 - updates the value of the variable
- * Description: the pointer points to to 98
+ * the pointer points to to 98
  * @n: pointer to the variable to update
  */
 void reset_to_98(int *n)
