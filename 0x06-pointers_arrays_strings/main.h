02200c605dc3d1b6990f96745d94b788bfee65f3
Aminax <aaminoxuseen45@gmail.com>
2022-07-13 22:58:46 -0700

    kk

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,13 @@
-int _putchar(char c)
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int _putchar(char c);
+void putchar_many(int n);
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -6,3 +15,5 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+
+#endif /* MAIN_H */
