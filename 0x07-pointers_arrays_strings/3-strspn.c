27319d4a1a0af8d3bc48e59d010e9c5d358de5db
Aminax <aaminoxuseen45@gmail.com>
2022-07-18 07:03:26 -0700

    file 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,12 +1,12 @@
-nclude "main.h"
+#include "main.h"
 
 /**
- *  * @brief: *_strspn - gets the length of a prefix substring
- *   * @s: string to evaluate
- *    * @accept: string containing the list of characters to match in s
+ * @brief: *_strspn - gets the length of a prefix substring
+ * @s: string to evaluate
+ *  @accept: string containing the list of characters to match in s
  *
- *      * Return: the number of bytes in the initial segment
- *       * of s which consist only of bytes from accept
+ * Return: the number of bytes in the initial segment
+ * of s which consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
