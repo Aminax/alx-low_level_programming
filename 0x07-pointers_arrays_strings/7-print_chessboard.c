d75b250d29bbc9d5393dd38d928c58a974f0af8d
Aminax <aaminoxuseen45@gmail.com>
2022-07-18 06:55:41 -0700

    file 7,8

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print chessboard
+ * @a: pointer to chessboard
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
+
+	for (y = 0; y < 8; y++)
+	{
+		for (x = 0; x < 8; x++)
+			_putchar(a[y][x]);
+		_putchar('\n');
+	}
+}
